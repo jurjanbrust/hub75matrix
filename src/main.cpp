c7033f4d14187c8aeecbf1be348a4e19e1c91fea
Jurjan Brust <jurjanbrust@hotmail.com>
2025-06-07 15:09:18 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,21 @@
 #define PANEL_RES_Y 32     // Number of pixels tall of each INDIVIDUAL panel module.
 #define PANEL_CHAIN 2      // Total number of panels chained one to another horizontally only.
 
+// #define R1_PIN 25
+// #define G1_PIN 26
+// #define B1_PIN 27
+// #define R2_PIN 14
+// #define G2_PIN 12
+// #define B2_PIN 13
+// #define A_PIN 23
+// #define B_PIN 19
+// #define C_PIN 5
+// #define D_PIN 17
+// #define E_PIN -1 // required for 1/32 scan panels, like 64x64px. Any available pin would do, i.e. IO32
+// #define LAT_PIN 4
+// #define OE_PIN 15
+// #define CLK_PIN 16
+
 //MatrixPanel_I2S_DMA dma_display;
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
